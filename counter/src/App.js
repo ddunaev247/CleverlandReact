1afe8ef9a7ee9dc24c325897cffcfdc54b57cbaa
ddunaev247 <daniil.dunaev247@gmail.com>
2023-02-28 23:26:19 +0300

    counter

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,23 +1,44 @@
 import { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <div className="counter">10</div>
-        <div className="controls">
-          <button>INC</button>
-          <button>DEC</button>
-          <button>RND</button>
-          <button>RESET</button>
-        </div>
-      </div>
-    );
-  }
+export class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: props.counter,
+        };
+    }
+    increment = () => {
+        if (this.state.counter < 10) {
+            this.setState(state => ({ counter: state.counter + 1 }));
+        }
+    };
+    decrement = () => {
+        if (this.state.counter > -10) {
+            this.setState(state => ({ counter: state.counter - 1 }));
+        }
+    };
+    showRondomCount = () => {
+        this.setState(() => ({
+            counter: +(Math.random() * (10 - -10) + -10).toFixed(0),
+        }));
+    };
+    resetCount = () => {
+        this.setState(() => ({
+            counter: this.props.counter,
+        }));
+    };
+    render() {
+        return (
+            <div className="app">
+                <div className="counter">{this.state.counter}</div>
+                <div className="controls">
+                    <button onClick={this.increment}>INC</button>
+                    <button onClick={this.decrement}>DEC</button>
+                    <button onClick={this.showRondomCount}>RND</button>
+                    <button onClick={this.resetCount}>RESET</button>
+                </div>
+            </div>
+        );
+    }
 }
-export default App;
