251b5bd6029dde475327c3ad232bf3645c8060f3
ddunaev247 <daniil.dunaev247@gmail.com>
2023-03-08 01:32:36 +0300

    toggleProp and app-info get data

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -4,20 +4,25 @@ import { SearchPanel } from '../search-panel/search-panel';
 import { AppFilter } from '../app-filter/app-filter';
 import { EmployeesList } from '../employees-list/employees-list';
 import { EmployeesAddForm } from '../employees-add-form/employees-add-form';
+import { employees } from '../../const/employee';
 import './app.css';
 
 export class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [
-                { name: 'Alex M.', salary: 1000, increase: false, id: 1 },
-                { name: 'John S.', salary: 7007, increase: true, id: 2 },
-                { name: 'Michael K.', salary: 5200, increase: false, id: 3 },
-            ],
+            data: employees,
+            allEmployees: employees.length,
+            rewarded: employees.filter(item => item.increase === true).length,
         };
-        this.maxId = this.state.data.length;
+        this.maxId = this.state.data.length + 1;
     }
+    getEmployees = () => {
+        this.setState(state => ({
+            allEmployees: state.data.length,
+            rewarded: state.data.filter(item => item.increase === true).length,
+        }));
+    };
     deleteItem = id => {
         this.setState(({ data }) => {
             // const index = data.findIndex(elem => elem.id === id);
@@ -28,17 +33,33 @@ export class App extends Component {
 
             return { data: data.filter(item => item.id !== id) };
         });
+        this.getEmployees();
     };
     addedItem = (name, salary) => {
         const newItem = { name, salary, increase: false, id: this.maxId++ };
         this.setState(({ data }) => ({
             data: [...data, newItem],
         }));
+        this.getEmployees();
+    };
+    onToggleProp = (id, prop) => {
+        this.setState(({ data }) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return { ...item, [prop]: !item[prop] };
+                }
+                return item;
+            }),
+        }));
+        this.getEmployees();
     };
     render() {
         return (
             <div className="app">
-                <AppInfo />
+                <AppInfo
+                    allEmployees={this.state.allEmployees}
+                    rewarded={this.state.rewarded}
+                />
                 <div className="search-panel">
                     <SearchPanel />
                     <AppFilter />
@@ -46,6 +67,7 @@ export class App extends Component {
                 <EmployeesList
                     dataEmployees={this.state.data}
                     onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}
                 />
                 <EmployeesAddForm addItem={this.addedItem} />
             </div>
