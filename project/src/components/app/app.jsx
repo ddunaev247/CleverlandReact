8d0fca79687a38585f18fbf1552835c5b375fa07
ddunaev247 <daniil.dunaev247@gmail.com>
2023-03-15 22:44:06 +0300

    refactor code

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,24 +1,24 @@
-import { Component } from 'react';
-import { AppInfo } from '../app-info/app-info';
-import { SearchPanel } from '../search-panel/search-panel';
-import { AppFilter } from '../app-filter/app-filter';
-import { EmployeesList } from '../employees-list/employees-list';
-import { EmployeesAddForm } from '../employees-add-form/employees-add-form';
-import './app.css';
+import { Component } from "react";
+import { AppInfo } from "../app-info/app-info";
+import { SearchPanel } from "../search-panel/search-panel";
+import { AppFilter } from "../app-filter/app-filter";
+import { EmployeesList } from "../employees-list/employees-list";
+import { EmployeesAddForm } from "../employees-add-form/employees-add-form";
+import "./app.css";
 
 export class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             data: [
-                { name: 'Alex M.', salary: 1000, increase: false, id: 1 },
-                { name: 'John S.', salary: 7007, increase: true, id: 2 },
-                { name: 'Michael K.', salary: 5200, increase: false, id: 3 },
+                { name: "Alex M.", salary: 1000, increase: false, id: 1 },
+                { name: "John S.", salary: 7007, increase: true, id: 2 },
+                { name: "Michael K.", salary: 5200, increase: false, id: 3 },
             ],
         };
         this.maxId = this.state.data.length + 1;
     }
-    deleteItem = id => {
+    deleteItem = (id) => {
         this.setState(({ data }) => {
             // const index = data.findIndex(elem => elem.id === id);
             // const before = data.slice(0, index);
@@ -26,7 +26,7 @@ export class App extends Component {
             // const newData = [...before, ...after];
             // return { data: newData };
 
-            return { data: data.filter(item => item.id !== id) };
+            return { data: data.filter((item) => item.id !== id) };
         });
     };
     addedItem = (name, salary) => {
