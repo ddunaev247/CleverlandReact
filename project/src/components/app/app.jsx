ec1dd54a5c65ccc9b89c3393e06bf1862eee42ae
ddunaev247 <daniil.dunaev247@gmail.com>
2023-03-15 22:43:14 +0300

    refactor code

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,11 +1,11 @@
-import { Component } from 'react';
-import { AppInfo } from '../app-info/app-info';
-import { SearchPanel } from '../search-panel/search-panel';
-import { AppFilter } from '../app-filter/app-filter';
-import { EmployeesList } from '../employees-list/employees-list';
-import { EmployeesAddForm } from '../employees-add-form/employees-add-form';
-import { employees } from '../../const/employee';
-import './app.css';
+import { Component } from "react";
+import { AppInfo } from "../app-info/app-info";
+import { SearchPanel } from "../search-panel/search-panel";
+import { AppFilter } from "../app-filter/app-filter";
+import { EmployeesList } from "../employees-list/employees-list";
+import { EmployeesAddForm } from "../employees-add-form/employees-add-form";
+import { employees } from "../../const/employee";
+import "./app.css";
 
 export class App extends Component {
     constructor(props) {
@@ -13,17 +13,18 @@ export class App extends Component {
         this.state = {
             data: employees,
             allEmployees: employees.length,
-            rewarded: employees.filter(item => item.increase === true).length,
+            rewarded: employees.filter((item) => item.increase === true).length,
         };
         this.maxId = this.state.data.length + 1;
     }
     getEmployees = () => {
-        this.setState(state => ({
+        this.setState((state) => ({
             allEmployees: state.data.length,
-            rewarded: state.data.filter(item => item.increase === true).length,
+            rewarded: state.data.filter((item) => item.increase === true)
+                .length,
         }));
     };
-    deleteItem = id => {
+    deleteItem = (id) => {
         this.setState(({ data }) => {
             // const index = data.findIndex(elem => elem.id === id);
             // const before = data.slice(0, index);
@@ -31,7 +32,7 @@ export class App extends Component {
             // const newData = [...before, ...after];
             // return { data: newData };
 
-            return { data: data.filter(item => item.id !== id) };
+            return { data: data.filter((item) => item.id !== id) };
         });
         this.getEmployees();
     };
@@ -44,7 +45,7 @@ export class App extends Component {
     };
     onToggleProp = (id, prop) => {
         this.setState(({ data }) => ({
-            data: data.map(item => {
+            data: data.map((item) => {
                 if (item.id === id) {
                     return { ...item, [prop]: !item[prop] };
                 }
